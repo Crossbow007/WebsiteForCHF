02ddde5f14f824c74dac7468609591a7e99b0943
Kevin Gao <kevin.gao@student.sl.on.ca>
2024-03-03 22:05:10 -0500

    login php change

--- login.php
+++ login.php
@@ -29,31 +29,7 @@ function performLogout() {
     exit;
 }
 
-// function AddRecordToTable($mysqlObj, $TableName)
-// {
-//     if (isset($_POST["name"], $_POST["email"], $_POST["message"])) {
-//         $name = $_POST["name"];
-//         $email = $_POST["email"];
-//         $message = $_POST["message"];
 
-//         $query = "INSERT INTO $TableName (name, email, message) VALUES (?, ?, ?)";
-//         $stmtObj = $mysqlObj->prepare($query);
-
-//         if ($stmtObj) {
-//             $stmtObj->bind_param("sss", $name, $email, $message);
-//             if ($stmtObj->execute()) {
-//                 echo "New record added successfully";
-//             } else {
-//                 echo "Error: " . $stmtObj->error;
-//             }
-//             $stmtObj->close();
-//         } else {
-//             echo "Failed to prepare the statement";
-//         }
-//     } else {
-//         echo "One or more POST parameters are missing";
-//     }
-// }
 
 
 
